826fab302cdd713078b0d8ad29af0a77e97c92ad
Vitor Martins Melo <seuemail@email.com>
2025-02-12 17:05:31 -0300

    Versao final do projeto

--- Semana(1,2)/PJ.java
+++ Semana(1,2)/PJ.java
@@ -1,11 +1,4 @@
 public class PJ extends Funcionario {
-    // public PJ(String nome, double salario, double bonus, double desconto) {
-    //     super(nome, salario, bonus, desconto);
-    // }
-
-    // public double calcularSalario() {
-    //     return getSalario() + getBonus();
-    // }
     public PJ(String nome, double salario, double bonos, double descontos){
         super(nome, salario, bonos, descontos);
     }
